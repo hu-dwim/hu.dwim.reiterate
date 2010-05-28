ff7a8a627b10fb24894ea8916d0594fc614fc745
attila.lendvai <attila.lendvai@gmail.com>
2010-05-28 11:31:54 +0000

    tests

--- test/basic.lisp
+++ test/basic.lisp
@@ -6,6 +6,24 @@
 
 (in-package :hu.dwim.reiterate.test)
 
+(def suite* (test/basic :in test))
+
+(def test test/basic/naming ()
+  (is (eq (name-of (walk-form/reiterate '(iter alma)))
+          'alma))
+  (is (eq (name-of (walk-form/reiterate '(iter named alma)))
+          'alma))
+  (finishes (macroexpand '(iter (for foo in-list '(1 2 3))
+                                (repeat 2)
+                                (print foo))))
+  (signals error (walk-form/reiterate '(iter named))))
+
+(def test test/basic/clauses ()
+  (finishes (macroexpand '(iter (for foo in-list '(1 2 3))
+                                (repeat 2)
+                                (print foo)))))
+
+#+nil
 (def test test/basic/1 ()
   (is (equal (iter (repeat 3)
                    (collect 1))
--- test/package.lisp
+++ test/package.lisp
@@ -7,11 +7,25 @@
 (in-package :hu.dwim.def)
 
 (def package :hu.dwim.reiterate.test
-  (:use :hu.dwim.common
+  (:use :alexandria
+        :anaphora
+        :contextl
+        :hu.dwim.common-lisp
         :hu.dwim.def
         :hu.dwim.defclass-star
         :hu.dwim.reiterate
         :hu.dwim.stefil
         :hu.dwim.syntax-sugar
-        :hu.dwim.util)
+        :hu.dwim.walker
+        :hu.dwim.util
+        :metabang-bind)
   (:readtable-setup (setup-readtable/same-as-package :hu.dwim.reiterate)))
+
+(in-package :hu.dwim.reiterate.test)
+
+(bind ((mohter-package (find-package :hu.dwim.reiterate))
+       (test-package (find-package :hu.dwim.reiterate.test)))
+  (do-symbols (symbol mohter-package)
+    (when (and (eq (symbol-package symbol) mohter-package)
+               (not (find-symbol (symbol-name symbol) test-package)))
+      (import symbol))))

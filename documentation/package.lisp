8df259ab6aa809fa0072f584bfed563e286cf729
levente.meszaros <levente.meszaros@gmail.com>
2010-08-02 09:27:40 +0000

    Fix documentation package.

--- documentation/package.lisp
+++ documentation/package.lisp
@@ -7,12 +7,15 @@
 (in-package :hu.dwim.def)
 
 (def package :hu.dwim.reiterate.documentation
-  (:use :hu.dwim.asdf
-        :hu.dwim.common
+  (:use :alexandria
+        :anaphora
+        :hu.dwim.asdf
+        :hu.dwim.common-lisp
         :hu.dwim.def
         :hu.dwim.defclass-star
         :hu.dwim.reiterate
         :hu.dwim.syntax-sugar
         :hu.dwim.util
+        :metabang-bind
         :hu.dwim.wui)
   (:readtable-setup (setup-readtable/same-as-package :hu.dwim.wui)))

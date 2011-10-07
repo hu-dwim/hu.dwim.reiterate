5cc3f3eaaddc0b822579350ce560b02ab4eadc78
attila.lendvai <attila.lendvai@gmail.com>
2011-10-07 16:53:17 +0000

    doc/ record some ideas/thoughts

--- documentation/main.lisp
+++ documentation/main.lisp
@@ -11,17 +11,31 @@
 
 (def book user-guide (:title "User guide")
   (chapter (:title "Introduction")
-    (paragraph ()
-      "TODO")
-    (chapter (:title "Origina of the name")
+    (chapter (:title "Origins of the name")
       (paragraph ()
-        "The reiterate name was contributed by _3b on #lisp, thanks!")))
-  (chapter (:title "Supported Common Lisp Implementations")
+        "The reiterate name was contributed by _3b on #lisp, thanks! It's a pun on iterate, which is a library that highly influenced the API of hu.dwim.reiterate.")))
+  (chapter (:title "TODO")
     (paragraph ()
-      "SBCL"))
-  (chapter (:title "Supported Operating Systems")
-    (paragraph ()
-      "Linux"))
-  (chapter (:title "Tutorial")
-    (paragraph ()
-      "TODO")))
+      "the TODO is to be filled up... :)")))
+
+#|
+
+attila: variable allocation and assignment should be flexible enough that most features should work with multiple values and destructuring bind. i'm not sure that that's the case already, but it's noted.
+
+Philipp Marek <philipp@marek.priv.at>
+> Eg. an easier way for multiple previous - eg. this works:
+>    (iterate:iter
+>      (for e in '(1 2 3 4 5 6 7 8 9))
+>      (for d previous e)
+>      (for c previous d)
+>      (for b previous c)
+>      (for a previous b)
+>      (print (list a b c d e)))
+>  but this doesn't:
+>    (iterate:iter
+>      (for e in '(1 2 3 4 5 6 7 8 9))
+>      (for (d c b a) previous (list e d c b))
+>      (print (list a b c d e)))
+
+
+|#
